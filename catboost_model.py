31af24cc8384862d9b7b5e1e6fc82460fd8202a6
8bithermitcrab <8bithermitcrab@gmail.com>
2022-07-08 11:49:17 +0900

    220708

--- catboost_model.py
+++ catboost_model.py
@@ -73,11 +73,9 @@ def preprocessing(train, test):
         df['주거_형태'].astype(str) + '_' + df['직업'].astype(str)
 
     # cols = ['자녀_수', '나이', '고용연수_r']
-    ## cols = ['자녀_수', '나이', '고용연수_r', '연령대', '소득_4분위', '소득_5분위', '소득_10분위', '신용도_r', 'Unnamed: 0']
-    cols = ['자녀_수', '고용연수_r', '연령대', '소득_4분위', '소득_5분위', '소득_10분위', '신용도_r', 'Unnamed: 0']
+    cols = ['자녀_수', '나이', '고용연수_r', '연령대', '소득_4분위', '소득_5분위', '소득_10분위', '신용도_r', 'Unnamed: 0']
     train.drop(cols, axis=1, inplace=True)
-    ## cols_t = ['자녀_수', '나이', '고용연수_r']
-    cols_t = ['자녀_수', '고용연수_r']
+    cols_t = ['자녀_수', '나이', '고용연수_r']
     test.drop(cols_t, axis=1, inplace=True)
 
     numerical_feats = df.dtypes[df.dtypes != "object"].index.tolist()
